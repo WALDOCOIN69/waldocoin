b99dcb166172f547983a2173d6c3b6b5691fff74
BBoy416 <waldocoin@outlook.com>
2025-05-13 17:57:30 -0400

    ♻️ Remove dotenv from xummClient.js for Render compatibility

--- waldocoin-backend/utils/xummClient.js
+++ waldocoin-backend/utils/xummClient.js
@@ -6,4 +6,7 @@ const xumm = new XummSdk(
   process.env.XUMM_API_SECRET
 );
 
+console.log("🔐 XUMM_API_KEY Present?", !!process.env.XUMM_API_KEY);
+console.log("🔐 XUMM_API_SECRET Present?", !!process.env.XUMM_API_SECRET);
+
 export default xumm;
