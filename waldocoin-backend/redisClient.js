9a6a8db14b97642d5ee8b89b2110b8e4fcd4f70a
BBoy416 <waldocoin@outlook.com>
2025-05-24 23:05:05 -0400

    🚀 Add working redisClient with connect

--- waldocoin-backend/redisClient.js
+++ waldocoin-backend/redisClient.js
@@ -2,13 +2,15 @@
 import { createClient } from "redis";
 
 const redis = createClient({
-url: process.env.REDIS_URL || "redis://localhost:6379"
+  url: process.env.REDIS_URL || "redis://localhost:6379"
 });
 
 redis.on("error", (err) => {
-console.error("❌ Redis Client Error", err);
+  console.error("❌ Redis Client Error", err);
 });
 
 await redis.connect();
+
+console.log("✅ Redis connected");
 
 export default redis;
