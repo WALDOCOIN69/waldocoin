33dda1002049e2761168c18f06df0b999694c085
BBoy416 <waldocoin@outlook.com>
2025-06-03 14:04:55 -0400

    �� Fix XUMM import: use CommonJS style destructuring for compatibility

--- waldocoin-backend/utils/xummClient.js
+++ waldocoin-backend/utils/xummClient.js
@@ -1,4 +1,6 @@
-import { Xumm } from "xumm-sdk";
+// utils/xummClient.js
+import pkg from "xumm-sdk";
+const { Xumm } = pkg;
 
 export const getXummClient = () =>
   new Xumm(process.env.XUMM_API_KEY, process.env.XUMM_API_SECRET);
