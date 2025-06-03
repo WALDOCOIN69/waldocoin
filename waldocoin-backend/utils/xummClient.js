30cc18822688294953e8481e10ab4cb5c6768391
BBoy416 <waldocoin@outlook.com>
2025-06-03 09:25:28 -0400

    �� FIX: Always return fresh XUMM client to fix 'client is closed'

--- waldocoin-backend/utils/xummClient.js
+++ waldocoin-backend/utils/xummClient.js
@@ -1,19 +1,9 @@
 // utils/xummClient.js
-import { XummSdk } from "xumm-sdk";
-import dotenv from "dotenv";
+import { Xumm } from 'xumm-sdk';
+import dotenv from 'dotenv';
 dotenv.config();
 
-export async function getXummClient() {
-  const { XUMM_API_KEY, XUMM_API_SECRET } = process.env;
-
-  if (!XUMM_API_KEY || !XUMM_API_SECRET) {
-    throw new Error("❌ Missing XUMM credentials");
-  }
-
-  const client = new XummSdk(XUMM_API_KEY, XUMM_API_SECRET);
-  console.log("🔥 New XUMM SDK client created");
-  return client;
+export default async function getXummClient() {
+  return new Xumm(process.env.XUMM_API_KEY, process.env.XUMM_API_SECRET);
 }
-
-export default getXummClient;
 
