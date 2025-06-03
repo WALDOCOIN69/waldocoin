77cbbe486d435427bc8114bc4e2b4d70c2453a30
BBoy416 <waldocoin@outlook.com>
2025-06-03 15:05:02 -0400

    �� Debug: added console log to confirm XUMM import

--- waldocoin-backend/utils/xummClient.js
+++ waldocoin-backend/utils/xummClient.js
@@ -1,6 +1,7 @@
-// utils/xummClient.js
 import pkg from "xumm-sdk";
 const { Xumm } = pkg;
+
+console.log("🧪 LOADED XUMM CLIENT CORRECTLY"); // <--- ADD THIS LINE
 
 export const getXummClient = () =>
   new Xumm(process.env.XUMM_API_KEY, process.env.XUMM_API_SECRET);
