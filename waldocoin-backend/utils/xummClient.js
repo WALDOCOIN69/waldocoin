e42dd92e201de4c2c5579204d781a8be2cf08f03
BBoy416 <waldocoin@outlook.com>
2025-06-03 14:01:15 -0400

    🚑 Fix getXummClient: correct Xumm import for ES module

--- waldocoin-backend/utils/xummClient.js
+++ waldocoin-backend/utils/xummClient.js
@@ -1,7 +1,6 @@
-// utils/xummClient.js
-import pkg from "xumm-sdk";
-const { Xumm } = pkg;
+import { Xumm } from "xumm-sdk";
 
 export const getXummClient = () =>
   new Xumm(process.env.XUMM_API_KEY, process.env.XUMM_API_SECRET);
+
 
