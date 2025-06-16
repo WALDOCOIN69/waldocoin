4411e4dba819d0f83ffa5e832cd51d533307af66
BBoy416 <waldocoin@outlook.com>
2025-06-16 13:24:12 -0400

    🐛 Fix Redis import path in mint/confirm.js

--- waldocoin-backend/routes/mint/confirm.js
+++ waldocoin-backend/routes/mint/confirm.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import xrpl from "xrpl";
-import { redis } from "../redisClient.js";
+import { redis } from "../../redisClient.js";
 import { uploadToIPFS } from "../utils/ipfsUploader.js";
 
 dotenv.config();
