1ba2cb14357a4197eab3aa4dd3a25f699962862e
BBoy416 <waldocoin@outlook.com>
2025-06-20 12:55:20 -0400

    🐛 Fix xummClient default import in battle start route

--- waldocoin-backend/routes/battle/start.js
+++ waldocoin-backend/routes/battle/start.js
@@ -3,7 +3,7 @@ import express from "express";
 import { v4 as uuidv4 } from "uuid";
 import dayjs from "dayjs";
 import { redis } from "../../redisClient.js";
-import { xummClient } from "../../utils/xummClient.js";
+import xummClient from "../../utils/xummClient.js"; // ✅ correct
 import dotenv from "dotenv";
 dotenv.config();
 
