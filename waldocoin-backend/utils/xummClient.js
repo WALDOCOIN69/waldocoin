10c77428303f895368351dc2c77b3602a7e0604e
BBoy416 <waldocoin@outlook.com>
2025-06-20 13:18:44 -0400

    �� Fix CommonJS import for xumm-sdk in ESM context

--- waldocoin-backend/utils/xummClient.js
+++ waldocoin-backend/utils/xummClient.js
@@ -1,8 +1,8 @@
 // utils/xummClient.js
-import { Xumm } from 'xumm-sdk';
+import pkg from 'xumm-sdk';
+const { Xumm } = pkg;
 
 const xummClient = new Xumm(process.env.XUMM_API_KEY, process.env.XUMM_API_SECRET);
 
 export { xummClient };
-
 
