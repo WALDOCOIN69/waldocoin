a34fd72c26d6d7f083883fc95f27c4bead0baa9e
BBoy416 <waldocoin@outlook.com>
2025-06-20 14:24:26 -0400

    ✅ Fix Xumm import (default export)

--- waldocoin-backend/utils/xummClient.js
+++ waldocoin-backend/utils/xummClient.js
@@ -1,5 +1,6 @@
-import pkg from 'xumm-sdk';
-const { Xumm } = pkg;
+// utils/xummClient.js
+import Xumm from 'xumm-sdk'
 
-const xummClient = new Xumm(process.env.XUMM_API_KEY, process.env.XUMM_API_SECRET);
-export default xummClient;
+const xummClient = new Xumm(process.env.XUMM_API_KEY, process.env.XUMM_API_SECRET)
+
+export default xummClient
