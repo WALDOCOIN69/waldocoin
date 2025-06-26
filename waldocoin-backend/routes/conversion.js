65f740ea9aeb4232cafd8da49014deacbebc79ce
BBoy416 <waldocoin@outlook.com>
2025-06-26 15:45:59 -0400

    Fix route casing and confirm route files

--- waldocoin-backend/routes/conversion.js
+++ waldocoin-backend/routes/conversion.js
@@ -3,7 +3,12 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  return res.json({ waldoToXRP: 0.01, waldoToUSD: 0.005 });
+  res.json({
+    success: true,
+    waldoToXrp: 0.01,
+    waldoToUsd: 0.007,
+  });
 });
 
 export default router;
+
