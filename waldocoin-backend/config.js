a0521dc998d60ff50f4a156f4dffedac2fcf502c
BBoy416 <waldocoin@outlook.com>
2025-06-27 16:27:51 -0400

    ✅ Added WALDOCOIN_TOKEN to config.js

--- waldocoin-backend/config.js
+++ waldocoin-backend/config.js
@@ -1,1 +1,2 @@
 export const WALDO_TREASURY = "rnWfL48YCknW6PYewFLKfMKUymHCfj3aww"; // or use process.env if you prefer
+export const WALDOCOIN_TOKEN = "WLO"; // or whatever the actual XRPL token code is
