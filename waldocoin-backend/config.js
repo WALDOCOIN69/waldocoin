a2229f1247d3b668ede395c1b1de94a76c3d4ae3
BBoy416 <waldocoin@outlook.com>
2025-06-27 16:29:48 -0400

    🐛 Add WALDO_DISTRIBUTOR_SECRET export to config.js

--- waldocoin-backend/config.js
+++ waldocoin-backend/config.js
@@ -1,2 +1,3 @@
 export const WALDO_TREASURY = "rnWfL48YCknW6PYewFLKfMKUymHCfj3aww"; // or use process.env if you prefer
 export const WALDOCOIN_TOKEN = "WLO"; // or whatever the actual XRPL token code is
+export const WALDO_DISTRIBUTOR_SECRET = process.env.WALDO_DISTRIBUTOR_SECRET;
