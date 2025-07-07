db2fa96ab3ddb3c8602545558b4bc6a1fdc2b805
BBoy416 <waldocoin@outlook.com>
2025-07-07 16:28:46 -0400

    🔧 Fix WALDO trustline and issuer setup for airdrop on mainnet

--- waldocoin-backend/constants.js
+++ waldocoin-backend/constants.js
@@ -1,5 +1,5 @@
 // constants.js
-export const WALDO_DISTRIBUTOR = "rMJMw3i7W4dxTBkLKSnkNETCGPeons2MVt";
+export const DISTRIBUTOR_WALLET = "rMJMw3i7W4dxTBkLKSnkNETCGPeons2MVt";
 export const WALDOCOIN_TOKEN = "WLO"; // WALDO token code
 export const WALDO_ISSUER = "rstjAWDiqKsUMhHqiJShRSkuaZ44TXZyDY"; // WALDO issuer address
 export const WALDO_DISTRIBUTOR_SECRET = process.env.WALDO_DISTRIBUTOR_SECRET;
